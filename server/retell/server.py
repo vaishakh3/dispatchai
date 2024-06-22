c68cfb81072d2ad6ba8821bd7402ec924f1ea739
Spike O'Carroll <19519553+spikecodes@users.noreply.github.com>
2024-06-22 12:03:31 -0700

    ✨ Initial Retell commit

---
+++ server/retell/server.py
@@ -0,0 +1,204 @@
+import json
+import os
+import asyncio
+from dotenv import load_dotenv
+from fastapi import APIRouter, FastAPI, Request, WebSocket, WebSocketDisconnect
+from fastapi.responses import JSONResponse, PlainTextResponse
+from concurrent.futures import TimeoutError as ConnectionTimeoutError
+from twilio.twiml.voice_response import VoiceResponse
+from retell import Retell
+from retell.resources.call import RegisterCallResponse
+from .custom_types import (
+    ConfigResponse,
+    ResponseRequiredRequest,
+)
+from .twilio_server import TwilioClient
+from .llm import LlmClient  # or use .llm_with_func_calling
+
+load_dotenv(override=True)
+app = FastAPI()
+retell = Retell(api_key=os.environ["RETELL_API_KEY"])
+
+# Custom Twilio if you want to use your own Twilio API Key
+twilio_client = TwilioClient()
+# twilio_client.create_phone_number(213, "68978b1c2935ff9c7d7107e61524d0bb")
+# twilio_client.delete_phone_number("+12133548310")
+# twilio_client.register_inbound_agent("+13392016322", "68978b1c2935ff9c7d7107e61524d0bb")
+# twilio_client.create_phone_call("+13392016322", "+14157122917", "68978b1c2935ff9c7d7107e61524d0bb")
+
+router = APIRouter(
+    prefix="/retell",
+    tags=["retell"],
+    responses={404: {"description": "Not found"}},
+)
+
+# Handle webhook from Retell server. This is used to receive events from Retell server.
+# Including call_started, call_ended, call_analyzed
+@app.post("/webhook")
+async def handle_webhook(request: Request):
+    try:
+        post_data = await request.json()
+        valid_signature = retell.verify(
+            json.dumps(post_data, separators=(",", ":")),
+            api_key=str(os.environ["RETELL_API_KEY"]),
+            signature=str(request.headers.get("X-Retell-Signature")),
+        )
+        if not valid_signature:
+            print(
+                "Received Unauthorized",
+                post_data["event"],
+                post_data["data"]["call_id"],
+            )
+            return JSONResponse(status_code=401, content={"message": "Unauthorized"})
+        if post_data["event"] == "call_started":
+            print("Call started event", post_data["data"]["call_id"])
+        elif post_data["event"] == "call_ended":
+            print("Call ended event", post_data["data"]["call_id"])
+        elif post_data["event"] == "call_analyzed":
+            print("Call analyzed event", post_data["data"]["call_id"])
+        else:
+            print("Unknown event", post_data["event"])
+        return JSONResponse(status_code=200, content={"received": True})
+    except Exception as err:
+        print(f"Error in webhook: {err}")
+        return JSONResponse(
+            status_code=500, content={"message": "Internal Server Error"}
+        )
+
+
+# Twilio voice webhook. This will be called whenever there is an incoming or outgoing call.
+# Register call with Retell at this stage and pass in returned call_id to Retell.
+@app.post("/twilio-voice-webhook/{agent_id_path}")
+async def handle_twilio_voice_webhook(request: Request, agent_id_path: str):
+    try:
+        # Check if it is machine
+        post_data = await request.form()
+        if "AnsweredBy" in post_data and post_data["AnsweredBy"] == "machine_start":
+            twilio_client.end_call(post_data["CallSid"])
+            return PlainTextResponse("")
+        elif "AnsweredBy" in post_data:
+            return PlainTextResponse("")
+
+        call_response: RegisterCallResponse = retell.call.register(
+            agent_id=agent_id_path,
+            audio_websocket_protocol="twilio",
+            audio_encoding="mulaw",
+            sample_rate=8000,  # Sample rate has to be 8000 for Twilio
+            from_number=post_data["From"],
+            to_number=post_data["To"],
+            metadata={
+                "twilio_call_sid": post_data["CallSid"],
+            },
+        )
+        print(f"Call response: {call_response}")
+
+        response = VoiceResponse()
+        start = response.connect()
+        start.stream(
+            url=f"wss://api.retellai.com/audio-websocket/{call_response.call_id}"
+        )
+        return PlainTextResponse(str(response), media_type="text/xml")
+    except Exception as err:
+        print(f"Error in twilio voice webhook: {err}")
+        return JSONResponse(
+            status_code=500, content={"message": "Internal Server Error"}
+        )
+
+
+# Only used for web call frontend to register call so that frontend don't need api key.
+# If you are using Retell through phone call, you don't need this API. Because
+# this.twilioClient.ListenTwilioVoiceWebhook() will include register-call in its function.
+@app.post("/register-call-on-your-server")
+async def handle_register_call(request: Request):
+    try:
+        post_data = await request.json()
+        call_response = retell.call.register(
+            agent_id=post_data["agent_id"],
+            audio_websocket_protocol="web",
+            audio_encoding="s16le",
+            sample_rate=post_data[
+                "sample_rate"
+            ],  # Sample rate has to be 8000 for Twilio
+        )
+        print(f"Call response: {call_response}")
+    except Exception as err:
+        print(f"Error in register call: {err}")
+        return JSONResponse(
+            status_code=500, content={"message": "Internal Server Error"}
+        )
+
+
+# Start a websocket server to exchange text input and output with Retell server. Retell server
+# will send over transcriptions and other information. This server here will be responsible for
+# generating responses with LLM and send back to Retell server.
+@app.websocket("/llm-websocket/{call_id}")
+async def websocket_handler(websocket: WebSocket, call_id: str):
+    try:
+        await websocket.accept()
+        llm_client = LlmClient()
+
+        # Send optional config to Retell server
+        config = ConfigResponse(
+            response_type="config",
+            config={
+                "auto_reconnect": True,
+                "call_details": True,
+            },
+            response_id=1,
+        )
+        await websocket.send_json(config.__dict__)
+
+        # Send first message to signal ready of server
+        response_id = 0
+        first_event = llm_client.draft_begin_message()
+        await websocket.send_json(first_event.__dict__)
+
+        async def handle_message(request_json):
+            nonlocal response_id
+
+            # There are 5 types of interaction_type: call_details, pingpong, update_only, response_required, and reminder_required.
+            # Not all of them need to be handled, only response_required and reminder_required.
+            if request_json["interaction_type"] == "call_details":
+                print(json.dumps(request_json, indent=2))
+                return
+            if request_json["interaction_type"] == "ping_pong":
+                await websocket.send_json(
+                    {
+                        "response_type": "ping_pong",
+                        "timestamp": request_json["timestamp"],
+                    }
+                )
+                return
+            if request_json["interaction_type"] == "update_only":
+                return
+            if (
+                request_json["interaction_type"] == "response_required"
+                or request_json["interaction_type"] == "reminder_required"
+            ):
+                response_id = request_json["response_id"]
+                request = ResponseRequiredRequest(
+                    interaction_type=request_json["interaction_type"],
+                    response_id=response_id,
+                    transcript=request_json["transcript"],
+                )
+                print(
+                    f"""Received interaction_type={request_json['interaction_type']}, response_id={response_id}, last_transcript={request_json['transcript'][-1]['content']}"""
+                )
+
+                async for event in llm_client.draft_response(request):
+                    await websocket.send_json(event.__dict__)
+                    if request.response_id < response_id:
+                        break  # new response needed, abandon this one
+
+        async for data in websocket.iter_json():
+            asyncio.create_task(handle_message(data))
+
+    except WebSocketDisconnect:
+        print(f"LLM WebSocket disconnected for {call_id}")
+    except ConnectionTimeoutError as e:
+        print("Connection timeout error for {call_id}")
+    except Exception as e:
+        print(f"Error in LLM WebSocket: {e} for {call_id}")
+        await websocket.close(1011, "Server error")
+    finally:
+        print(f"LLM WebSocket connection closed for {call_id}")
